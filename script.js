23c699f146924276dd8bd0ce13cba4d925ebef5a
AdrianCreate <adrianalexandru1307@gmail.com>
2023-08-18 18:07:22 +0300

    Finish game function - working program

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-function getComputerChoice () {
-    let num = Math.floor(Math.random()*3);
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
     return num == 0 ? "Rock"
-    : num == 1 ? "Paper"
-    : "Scrissors" ;
+        : num == 1 ? "Paper"
+            : "Scrissors";
 }
 
-function playRound (playerSelection , computerSelection) {
+function playRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-        console.log ("It's a tie! Nobody wins!");
+        console.log("It's a tie! Nobody wins!");
         return null
     }
     else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase() === 'PAPER') {
-     console.log("You Lose! Paper beats Rock");
-     return false
+        console.log("You Lose! Paper beats Rock");
+        return false
 
     }
     else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection.toUpperCase() === 'ROCK') {
-     console.log("You Win! Paper beats Rock");
-     return true
+        console.log("You Win! Paper beats Rock");
+        return true
     }
     else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase() === 'SCRISSORS') {
-     console.log("You Win! Rock beats Scrissors");
-     return true
+        console.log("You Win! Rock beats Scrissors");
+        return true
     }
     else if (playerSelection.toUpperCase() === 'SCRISSORS' && computerSelection.toUpperCase() === 'ROCK') {
-     console.log("You Lose! Rock beats Scrissors");
-     return false
+        console.log("You Lose! Rock beats Scrissors");
+        return false
     }
     else if (playerSelection.toUpperCase() === 'SCRISSORS' && computerSelection.toUpperCase() === 'PAPER') {
-     console.log("You Win! Scrissors beats Paper");
-     return true
+        console.log("You Win! Scrissors beats Paper");
+        return true
     }
     else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection.toUpperCase() === 'SCRISSORS') {
-     console.log("You Lose! Scrissors beats Paper");
-     return false
+        console.log("You Lose! Scrissors beats Paper");
+        return false
     }
     else {
         console.log("Not a valid input");
@@ -41,9 +41,41 @@ function playRound (playerSelection , computerSelection) {
     }
 }
 
+function game() {
+    let playerSelection, computerSelection, result;
+    let playerWins = 0;
+    let computerWins = 0;
+    const selections = ["ROCK", "PAPER", "SCRISSORS"];
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("What's your choice?");
+        computerSelection = getComputerChoice();
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-console.log(playerSelection);
-console.log(computerSelection);
+        while (!(selections.includes(playerSelection.toUpperCase()))) {
+            playerSelection = prompt();
+        }
+
+        console.log(`User plays ${playerSelection}, Computer plays ${computerSelection}`);
+        result = playRound(playerSelection, computerSelection);
+
+        if (result) {
+            playerWins += 1;
+        }
+        else if (result == false) {
+            computerWins += 1;
+        }
+        console.log(`Current score - Player: ${playerWins}, Computer: ${computerWins}`)
+    }
+
+    if (playerWins > computerWins) {
+        return "User wins!"
+    }
+    else if (computerWins > playerWins) {
+        return "Computer wins!"
+    }
+    else {
+        return "It's a tie! Play again?"
+    }
+
+}
+
+console.log(game());
